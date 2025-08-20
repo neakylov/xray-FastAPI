524fb001fad147b7cda171535f319d0f79f38b70
neakylov <akula.kirya@inbox.ru>
2025-08-20 12:14:07 +0300

    Реализовано добавление клиента

--- src/cryptway/setup.py
+++
@@ -1,5 +0,0 @@
-from passlib.context import CryptContext
-
-pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
-
-print(pwd_context.hash("cryptway_sk_ddf3708c9e0c50f70c60dea902f158859e988b123e6443b0bd503a59c53821e8"))
--- src/cryptway/xray/service.py
+++ src/cryptway/xray/service.py
@@ -3,6 +3,8 @@ import json
 import os
 import uuid
 
+from typing import Any
+
 from .models import Client
 
 def generate_uuid_as_str() -> str:
@@ -10,7 +12,17 @@ def generate_uuid_as_str() -> str:
 
 
 def get_config_path() -> str:
-    return os.path.join("usr", "local", "etc", "xray", "config.json")
+    return os.path.join("/", "usr", "local", "etc", "xray", "config.json")
+
+
+def make_client() -> Client:
+    client_id = generate_uuid_as_str()
+    flow = "xtls-rprx-vision"
+
+    return Client(
+        uuid=client_id,
+        flow=flow
+    )
 
 
 async def load_config(path: str):
@@ -20,22 +32,23 @@ async def load_config(path: str):
         return json.loads(cfg)
 
 
-async def update_config(path: str):
+async def update_config(path: str, old_config: Any, client: Client):
+    old_config["inbounds"][1]["clients"].append(
+        {
+            "id": client.uuid,
+            "flow": client.flow
+        }
+    )
     async with aiofiles.open(path, "w") as config:
-        await config.write()
+        await config.write(json.dumps(old_config, indent=4))
 
 
 async def add_new_client() -> Client:
     path = get_config_path()
 
-    uuid = generate_uuid_as_str()
-    flow = "xtls-rprx-vision"
+    client = make_client()
+    old_config = await load_config(path=path)
 
-    config = await load_config(path=path)
+    await update_config(path=path, old_config=old_config, client=client)
 
-    config["inbounds"][1]["clients"].append(
-        {
-            "id": uuid,
-            "flow": flow
-        }
-    )
+    return client
